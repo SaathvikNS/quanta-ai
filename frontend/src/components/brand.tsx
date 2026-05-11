d844cc2c891b0f5218f5c79f00a780a3f657b498
Saathvik N S <saanasha02@gmail.com>
2026-05-12 02:03:06 +0530

    feat: initial frontend container setup and landing page implemented

---
+++ frontend/src/components/brand.tsx
@@ -0,0 +1,34 @@
+// import Image from "next/image";
+import Link from "next/link";
+import Logo from "./Logo";
+
+export function Brand({ className = "" }: { className?: string }) {
+	return (
+		<Link href={"/"} className={`flex items-center gap-2 ${className}`}>
+			<div className="relative h-7 w-7">
+				{/* <div className="absolute inset-0 rounded-md bg-linear-to-br from-primary to-info opacity-90" />
+				<div className="absolute inset-0.75 rounded-[5px] bg-background flex items-center justify-center">
+					<span className="font-mono text-[10px] font-bold tracking-tighter">
+						Q
+					</span>
+				</div> */}
+				<Logo />
+
+				{/* <Image
+					src={"/favicon.ico"}
+					alt="brand-logo"
+					width={500}
+					height={500}
+				/> */}
+			</div>
+			<div className="flex flex-col leading-none">
+				<span className="text-sm font-semibold tracking-tight">
+					QUANTA
+				</span>
+				<span className="font-mono text-[9px] uppercase tracking-[0.2em] text-muted-foreground">
+					Intelligence
+				</span>
+			</div>
+		</Link>
+	);
+}
